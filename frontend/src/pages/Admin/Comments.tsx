3af7ded37c41e39464573def64e5d54921cce709
Kiruthicksan <kiruthicksanboopathi@gmail.com>
2025-10-30 21:14:19 +0530

    Finished Manage blog pag0

--- frontend/src/pages/Admin/Comments.tsx
+++ frontend/src/pages/Admin/Comments.tsx
@@ -1,6 +1,69 @@
+import { useEffect, useState } from "react";
+import type { CommentsData } from "../BlogPage";
+import { comments_data } from "@/assets/assets";
+
 const Comments = () => {
+  const [comments, setComments] = useState<CommentsData[]>([]);
+  const [fitler, setFilter] = useState("Not Approved");
+
+  const fetchComments = () => {
+    setComments(comments_data);
+  };
+
+  useEffect(() => {
+    fetchComments();
+  }, []);
   return (
-    <div>Comments</div>
-  )
-}
-export default Comments
+    <div className="flex-1 pt-5 px-5 sm:pt-12 sm:pl-16 bg-blue-50/50">
+      <div className="flex justify-between items-center max-w-3xl mb-4">
+        <h1>Comments</h1>
+        <div className="flex gap-4">
+          <button
+            className={`py-1 shadow-custom-sm border rounded-full px-4  text-xs cursor-pointer ${
+              fitler === "Approved"
+                ? "text-primary  border-primary "
+                : "text-gray-700"
+            }`}
+            onClick={() => setFilter("Approved")}
+          >
+            Approved
+          </button>
+          <button
+            className={`py-1 shadow-custom-sm border rounded-full px-4  text-xs cursor-pointer ${
+              fitler === "Not Approved"
+                ? "text-primary  border-primary"
+                : "text-gray-700"
+            }`}
+            onClick={() => setFilter("Not Approved")}
+          >
+            Not Approved
+          </button>
+        </div>
+      </div>
+
+      <div className="relative h-4/5 max-w-4xl overflow-x-auto  shadow rounded-lg scrollbar-hide bg-white">
+        <table className="w-full text-sm text-gray-500">
+          <thead className="text-xs text-gray-700 text-left uppercase">
+            <tr>
+              <th scope="col" className="px-6 py-3">
+                Blog Title & Comment
+              </th>
+              <th scope="col" className="px-6 py-3 max-sm:hidden">
+                Date
+              </th>
+
+              <th scope="col" className="px-6 py-3">
+                Action
+              </th>
+            </tr>
+          </thead>
+
+          <tbody>
+
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+};
+export default Comments;
--- frontend/src/pages/Admin/ListBlog.tsx
+++ frontend/src/pages/Admin/ListBlog.tsx
@@ -1,6 +1,41 @@
+import { useEffect, useState } from "react";
+import type { BlogData } from "../BlogPage";
+import { blog_data } from "@/assets/assets";
+import TableData from "@/components/Admin/TableData";
+
 const ListBlog = () => {
+  const [blogs, setBlogs] = useState<BlogData[]>([]);
+
+  const fetchBlogs = async () => {
+    setBlogs(blog_data);
+  };
+
+  useEffect(() => {
+    fetchBlogs(), [];
+  });
+
   return (
-    <div>ListBlog</div>
-  )
-}
-export default ListBlog
+    <div className="flex-1 pt-5 px-5 sm:pt-12 sm:pl-16 bg-blue-50/50">
+      <h1 className="mb-4">All Blogs</h1>
+       <div className="relative h-4/5 max-w-4xl overflow-x-auto  shadow rounded-lg scrollbar-hide bg-white">
+          <table className="w-full text-sm text-gray-500" > 
+            <thead className="text-xs text-gray-600 text-left uppercase" >
+              <tr>
+                <th scope="col" className="px-2 py-4 xl:px-6 ">#</th>
+                <th scope="col" className="px-2 py-4 ">BlogTitle</th>
+                <th scope="col" className="px-2 py-4 max-sm:hidden  ">Date</th>
+                <th scope="col" className="px-2 py-4  max-sm:hidden">Status</th>
+                <th scope="col" className="px-2 py-4 ">Actions</th>
+              </tr>
+            </thead>
+            <tbody>
+              {blogs.map((blog, index) => (
+                  <TableData key={blog._id} blog={blog} fetchBlogs={fetchBlogs} index={index + 1}/>
+              ))}
+            </tbody>
+          </table>
+        </div>
+    </div>
+  );
+};
+export default ListBlog;
